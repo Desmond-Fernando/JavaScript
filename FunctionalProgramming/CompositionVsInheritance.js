6baff3425c050777fa0ccec70e06d8211db51f84
Desmond-Fernando <desmond.vinod@gmail.com>
2024-04-07 22:56:30 +0100

    OOPVsFP

--- FunctionalProgramming/CompositionVsInheritance.js
+++ FunctionalProgramming/CompositionVsInheritance.js
@@ -4,5 +4,17 @@
 // Fragile base Class problem
 // Hierachichy
 
+// few operations on common data
+// stateful
+// side effects
+// Imperative
+
+/////////////////
 // Composition
 //  What it has
+
+// Many operations on fixed data
+// stateless
+// pure
+// declarative
+// Suaitable for big data where it can run in parall
